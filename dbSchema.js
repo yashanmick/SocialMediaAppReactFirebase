f6132f0f01e3c1ef6d3d979c1b8fa1c30ec10196
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-21 17:16:27 +0530

    screams schema

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-08-21T10:30:40.527Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
