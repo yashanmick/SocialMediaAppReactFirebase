66ebb96a50d810095cbbd47dc8b4f280aeadbc1e
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-22 16:46:24 +0530

    login validation

--- socialMediaApp/socialMediaAppFunctions/functions/index.js
+++ socialMediaApp/socialMediaAppFunctions/functions/index.js
@@ -167,5 +167,42 @@ app.post('/signup', (req, res) => {
         });
 });
 
+
+//login route
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    //login validation
+    if (isEmpty(user.email)) {
+        errors.email = 'Must not be empty'
+    }
+
+    if (isEmpty(user.password)) {
+        errors.password = 'Must not be empty'
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+    }
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.getIdToken();
+        })
+        .then((token) => {
+            return res.json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+
+})
+
 // https://baseurl.com/api/something
 exports.api = functions.region('asia-south1').https.onRequest(app);
