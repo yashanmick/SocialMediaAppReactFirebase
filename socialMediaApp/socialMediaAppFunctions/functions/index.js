0fe6fd4bd3feaebae96a4b04dc45fa69867c57d5
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-22 16:19:33 +0530

    email validation

--- socialMediaApp/socialMediaAppFunctions/functions/index.js
+++ socialMediaApp/socialMediaAppFunctions/functions/index.js
@@ -71,6 +71,20 @@ app.post('/scream', (req, res) => {
 
 });
 
+//validating email
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
+//helper function to check the string empty or not
+const isEmpty = (string) => {
+    //eleminate any white spaces
+    if (string.trim() === '') return true;
+    else return false;
+}
+
 //signup route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -79,7 +93,21 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     };
-    //TODO: validate data
+
+    let errors = {};        //initializing errors object
+
+    //email validation
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Email must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) {
+        errors.password = 'Password must not be empty'
+    }
+
+
     let token, userId;
 
     //checking the handle being unique
