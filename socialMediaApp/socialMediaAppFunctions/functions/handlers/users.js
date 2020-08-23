91f72f203be06d8c364e4884510ae5a0e5554671
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-23 19:42:50 +0530

    image upload functionality

--- socialMediaApp/socialMediaAppFunctions/functions/handlers/users.js
+++ socialMediaApp/socialMediaAppFunctions/functions/handlers/users.js
@@ -1,5 +1,7 @@
 const { admin, db } = require('../util/admin');
 
+const { uuid } = require("uuidv4");
+
 const config = require('../util/config');
 
 const firebase = require('firebase');
@@ -116,46 +118,52 @@ exports.uploadImage = (req, res) => {
 
     const busboy = new BusBoy({ headers: req.headers });
 
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+        console.log(fieldname, file, filename, encoding, mimetype);
         if (mimetype !== 'image/png' && mimetype !== 'image/jpeg') {
             return res.status(400).json({ error: 'Wrong file type submitted' });
         }
         // my.image.png => ['my', 'image', 'png']
-        const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        const imageExtension = filename.split(".")[filename.split(".").length - 1];
         //ex : 4544545645456.png
-        imageFileName = `${Math.round(Math.random() * 1000000000).toString()}.${imageExtension}`;
+        imageFileName = `${Math.round(
+            Math.random() * 1000000000000
+        ).toString()}.${imageExtension}`;
         const filepath = path.join(os.tmpdir(), imageFileName);
         imageToBeUploaded = { filepath, mimetype };
         //for now the file(object) is created, so need to use the filesystem library to actually create this file
         file.pipe(fs.createWriteStream(filepath));
     });
-    busboy.on('finish', () => {
-        admin.storage().bucket().upload(imageToBeUploaded.filepath, {
-            resumable: false,
-            metadata: {
+    busboy.on("finish", () => {
+        admin
+            .storage()
+            .bucket()
+            .upload(imageToBeUploaded.filepath, {
+                resumable: false,
                 metadata: {
-                    contentType: imageToBeUploaded.mimetype,
-                    //Generate token to be appended to imageUrl
-                    firebaseStorageDownloadTokens: generatedToken,
-                }
-            }
-        })
+                    metadata: {
+                        contentType: imageToBeUploaded.mimetype,
+                        //Generate token to be appended to imageUrl
+                        firebaseStorageDownloadTokens: generatedToken,
+                    },
+                },
+            })
             .then(() => {
                 //construct the image url to added it to the user
                 //https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName} 
                 // will just download the file to the computer instead of showing it onthe brower
                 //adding 'alt.media' will shows the image on the browser
-                const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+                const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
 
                 //then add this url to our users user document 
                 return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
             })
             .then(() => {
-                return res.json({ message: 'Image uploaded successfully' });
+                return res.json({ message: "image uploaded successfully" });
             })
-            .catch(err => {
+            .catch((err) => {
                 console.error(err);
-                return res.status(500).json({ error: err.code });
+                return res.status(500).json({ error: "something went wrong" });
             });
     });
     busboy.end(req.rawBody);
