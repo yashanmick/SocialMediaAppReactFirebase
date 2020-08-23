7dd0983b1ef24e942a192544fbab656ba34a973c
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-24 01:02:26 +0530

    get a scream function

--- socialMediaApp/socialMediaAppFunctions/functions/handlers/screams.js
+++ socialMediaApp/socialMediaAppFunctions/functions/handlers/screams.js
@@ -49,28 +49,34 @@ exports.postOneScream = (req, res) => {
 
 }
 
-//get a scream
-exports.getScream = () => {
+//for complex firebase query we need to create an index for it in firebase server
+// Fetch one scream
+exports.getScream = (req, res) => {
     let screamData = {};
-    db.doc(`/screams/${req.params.screamId}`).get()
-        .then(doc => {
+    db.doc(`/screams/${req.params.screamId}`)
+        .get()
+        .then((doc) => {
             if (!doc.exists) {
-                return res.status(404).json({ error: "Scream not found" });
+                return res.status(404).json({ error: 'Scream not found' });
             }
             screamData = doc.data();
-            screamData.screamId = doc.id;   //adding id to the scream
-            return db.colleciotn('comments').where('screamId', '==', req.params.screamId).get();          //get all comments
+            screamData.screamId = doc.id;
+            return db
+                .collection('comments')
+                .orderBy('createdAt', 'desc')
+                .where('screamId', '==', req.params.screamId)
+                .get();
         })
         .then((data) => {
             screamData.comments = [];
-            data.forEach(doc => {
+            data.forEach((doc) => {
                 screamData.comments.push(doc.data());
             });
             return res.json(screamData);
         })
-        .catch(err => {
+        .catch((err) => {
             console.error(err);
-            res.status(500).json({ error: err.code }); p
-        })
-}
+            res.status(500).json({ error: 'Something went wrong' });
+        });
+};
 
