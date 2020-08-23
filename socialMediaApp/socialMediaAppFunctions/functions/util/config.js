594126e3f9e547d22d1e6c82551494129e0cbf35
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-23 14:54:54 +0530

    config

---
+++ socialMediaApp/socialMediaAppFunctions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCunw6AIpPj8xaKLzs_Dfr5pXaQRmLNVDQ",
+    authDomain: "socialmediaapp-cfc19.firebaseapp.com",
+    databaseURL: "https://socialmediaapp-cfc19.firebaseio.com",
+    projectId: "socialmediaapp-cfc19",
+    storageBucket: "socialmediaapp-cfc19.appspot.com",
+    messagingSenderId: "542464735807",
+    appId: "1:542464735807:web:b580b979ff1d1ececed68a",
+    measurementId: "G-H7KTMFE5QQ"
+};
