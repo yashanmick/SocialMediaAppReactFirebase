7311a03249d10396c2f29292314a3ff759cc5ff8
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-25 01:33:49 +0530

    like a scream & unlike a scream functionality

--- socialMediaApp/socialMediaAppFunctions/functions/handlers/screams.js
+++ socialMediaApp/socialMediaAppFunctions/functions/handlers/screams.js
@@ -33,13 +33,19 @@ exports.postOneScream = (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()         //date format to string
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),       //date format to string
+        likeCount: 0,
+        commentCount: 0
     };
     db
         .collection('screams')
         .add(newScream)
         .then(doc => {
-            res.json({ message: `doucment ${doc.id} created successfully` });
+            const resScream = newScream;
+            resScream.screamId = doc.id;
+            res.json(resScream);
+            //res.json({ message: `doucment ${doc.id} created successfully` });
 
         })
         .catch(err => {
@@ -113,3 +119,94 @@ exports.commentOnScream = (req, res) => {
         });
 };
 
+//like a scream
+exports.likeScream = (req, res) => {
+    const likeDocument = db.collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData;
+
+    screamDocument.get()
+        .then(doc => {
+            if (doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else {
+                return res.status(404).json({ error: 'Scream not found' });
+            }
+        })
+        //getting a query snapshot
+        .then(data => {
+            if (data.empty) {
+                return db.collection('likes')
+                    .add({
+                        screamId: req.params.screamId,
+                        userHandle: req.user.handle
+                    })
+                    .then(() => {
+                        screamData.likeCount++
+                        return screamDocument.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => {
+                        return res.json(screamData);
+                    })
+            }
+            else {
+                return res.status(400).json({ error: 'Scream already liked' });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code })
+        });
+}
+
+//unlike a scream
+exports.unlikeScream = (req, res) => {
+    const likeDocument = db
+        .collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData;
+
+    screamDocument
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else {
+                return res.status(404).json({ error: 'Scream not found' });
+            }
+        })
+        .then((data) => {
+            if (data.empty) {
+                return res.status(400).json({ error: 'Scream not liked' });
+            } else {
+                return db
+                    .doc(`/likes/${data.docs[0].id}`)
+                    .delete()
+                    .then(() => {
+                        screamData.likeCount--;
+                        return screamDocument.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => {
+                        res.json(screamData);
+                    });
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: err.code });
+        });
+};
