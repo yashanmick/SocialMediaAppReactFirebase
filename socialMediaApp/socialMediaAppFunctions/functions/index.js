24bfd80182504f6e9c21f041c6fac7a67e7536da
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-25 10:03:38 +0530

    deleteScream middleware

--- socialMediaApp/socialMediaAppFunctions/functions/index.js
+++ socialMediaApp/socialMediaAppFunctions/functions/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 
@@ -17,7 +17,7 @@ const { ResultStorage } = require('firebase-functions/lib/providers/testLab');
 app.get('/screams', getAllScreams);     //get all screams
 app.post('/scream', FBAuth, postOneScream);     //post a scream
 app.get('/scream/:screamId', getScream);
-app.delete('/scream/:screamId/delete', FBAuth, deleteScream);   //deleteScream
+app.delete('/scream/:screamId', FBAuth, deleteScream);   //deleteScream
 app.get('/scream/:screamId/like', FBAuth, likeScream);  //like a scream
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);  //unlike a scream
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);   //comment on scream
