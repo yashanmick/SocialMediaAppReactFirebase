e27776e1e74e3ee78acdec45a543604dc87099d0
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-26 17:48:00 +0530

    setting up routers for home, login, signup

--- socialMediaApp-client/social-media-app-client/src/App.js
+++ socialMediaApp-client/social-media-app-client/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Our app</h1>
-    </div>
-  );
+//pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" Component={home} />
+            <Route path="/login" Component={login} />
+            <Route path="/signup" Component={signup} />
+          </Switch>
+        </Router>
+
+      </div>
+    )
+  }
 }
+
+export default App
+
 
 export default App;
