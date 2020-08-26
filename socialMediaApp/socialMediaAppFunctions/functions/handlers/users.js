4826e296cd408f57af9425837c0bf3d8a56960e9
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-26 12:18:13 +0530

    modifications on error codes

--- socialMediaApp/socialMediaAppFunctions/functions/handlers/users.js
+++ socialMediaApp/socialMediaAppFunctions/functions/handlers/users.js
@@ -66,7 +66,7 @@ exports.signup = (req, res) => {
             if (err.code === 'auth/email-already-in-use') {
                 return res.status(400).json({ email: 'Email is already in use' })
             } else {
-                return res.status(500).json({ error: err.code });
+                return res.status(500).json({ general: 'something went wrong, Please try again' });
             }
         });
 }
@@ -94,10 +94,12 @@ exports.login = (req, res) => {
         })
         .catch(err => {
             console.error(err);
+            //auth/wrong-password
+            //auth/user-not-user
             if (err.code === 'auth/wrong-password') {
                 return res.status(403).json({ general: 'Wrong credentials, please try again' });
             } else {
-                return res.status(500).json({ error: err.code });
+                return res.status(500).json({ general: 'Something went wrong, Please try again' });
             }
 
         });
