936d8f83a6f59a860b3e5c38bea5f39fd160a641
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-30 02:18:27 +0530

    user authentication and signup with redux

--- frontend/socialapp-client/src/App.js
+++ frontend/socialapp-client/src/App.js
@@ -5,11 +5,12 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';   
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';   //create theme function
 import themeFile from './util/theme';
 import jwtDecode from 'jwt-decode';
+import axios from 'axios';
 //Redux - to bring the containers of our states
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
-
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 //components
 import Navbar from './components/Navbar';
@@ -19,20 +20,22 @@ import AuthRoute from './util/AuthRoute';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import Axios from 'axios';
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   // console.log(decodedToken);
   //check if the token is expired
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = '/login'
-    authenticated = false;
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;    //in case of refreshing page
+    store.dispatch(getUserData());
   }
 
 }
@@ -51,11 +54,11 @@ function App() {
               <AuthRoute
                 exact path='/login'
                 component={login}
-                authenticated={authenticated} />
+              />
               <AuthRoute
                 exact path='/signup'
                 component={signup}
-                authenticated={authenticated} />
+              />
             </Switch>
           </div>
         </Router>
