9f81f07bdaec65f4363c6a3eafa746bd2670ba4f
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-29 14:52:40 +0530

    ui

--- frontend/socialapp-client/src/pages/home.js
+++ frontend/socialapp-client/src/pages/home.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
+import Scream from '../components/Scream';
+
 class home extends Component {
+    //save those screams on state object
+    state = {
+        screams: null
+    }
     //fetching screams from the server
     componentDidMount() {
-
+        axios.get('/screams')
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    screams: res.data
+                });
+            })
+            .catch(err => console.log(err));
     }
+    //add Mui Typography on app.js
     render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map((scream) => <Scream scream={scream} />)
+        ) : <p>Loading..</p>
         return (
             <Grid container spacing={16}>
                 <Grid item sm={8} xs={12}>
-                    <p>Content..</p>
+                    {recentScreamsMarkup}
                 </Grid>
                 <Grid item sm={4} xs={12}>
                     <p>Profile</p>
